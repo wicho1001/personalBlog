3c4312ab09e7b4269b0394ced5774c28531f6659
wicho1001 <wichoguerrero@yahoo.com>
2020-08-28 23:02:23 -0500

    feat: Adding styles to navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,58 +3,16 @@
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+<nav class="flex justify-center h-25 bg-dark-800">
+	<ul class="flex items-center justify-between w-10/12 py-3">
+		<a href="." class="h-25">
+			<img class="h-full" src="wicho.png" alt="wicho" srcset="wicho.png">
+		</a>
+		<div class="flex">
+			<li class="px-6 py-1 {segment === undefined ? 'bg-primary-500 text-white rounded' : 'text-primary-50'}"><a href="." sapper:noscroll>Inicio</a></li>
+			<li class="px-6 py-1 {segment === 'blog' ? 'bg-primary-500 text-white rounded' : 'text-primary-50'}"><a rel=prefetch href="blog" sapper:noscroll>Blog</a></li>
+		</div>
 	</ul>
 </nav>
