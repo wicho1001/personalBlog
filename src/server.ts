268bf08c883b2d2a9f9ff9e17c0e005844f41e9c
wicho1001 <wichoguerrero@yahoo.com>
2020-09-03 22:46:50 -0500

    fix: Removing vercel for add netlify

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-(module.exports = express())
+express()
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
