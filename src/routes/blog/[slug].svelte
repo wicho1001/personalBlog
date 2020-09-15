94cc701df8436c5ad00221e5af9a8eb513ffeeb6
wicho1001 <wichoguerrero@yahoo.com>
2020-09-15 13:10:05 -0500

    feat: Adding onmount to disqus component

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -15,8 +15,15 @@
 
 <script>
   import DisqusSvelte from "disqus-svelte"; // You can use any alias.
+  import { onMount } from "svelte";
   export let post;
 
+  onMount(() => {
+    const d = document, s = d.createElement('script');
+    s.src = 'https://w1ch0-com.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', `${new Date()}`);
+    (d.head || d.body).appendChild(s);
+  });
 </script>
 
 <style>
@@ -30,6 +37,7 @@
   <h1 class="font-montserrat font-bold xs:text-4xl md:text-6xl text-primary-50 xs:w-full md:w-8/12 capitalize">{post.title}</h1>
   <div class='xs:w-full md:w-7/12'>
     {@html post.html}
-    <DisqusSvelte identifier="my-blog-post-41" />
-  </div>
+    <div class="my-6" id="disqus_thread"></div>      
+      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> 
+    </div>
 </div>
