5c867805ddfcb63077f8a5ee54e48bea51300967
wicho1001 <wichoguerrero747@gmail.com>
2021-08-14 20:05:27 -0500

    feat: added new pages template

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import "../app.postcss";
+  import Nav from '../components/Nav.svelte';
+  import FooterComponent from '../components/Footer.svelte';
+  import { theme } from "../store/store";
+</script>
+
+<svelte:head>
+  {#if $theme === 'dark'}
+    <link rel="stylesheet" href="/variables.darkmode.css">
+    {:else}
+    <link rel="stylesheet" href="/variables.lightmode.css">
+  {/if}
+</svelte:head>
+
+<main class="flex flex-col h-screen overflow-y-scroll px-4 bg-neutral">
+  <Nav></Nav>
+  <section class="flex-grow xs:w-full md:w-9/12 mx-auto">
+    <slot></slot>
+  </section>
+  <FooterComponent></FooterComponent>
+</main>
