1f2995d11313bc61650f7c69fc9954446984b90b
wicho1001 <wichoguerrero747@gmail.com>
2021-08-14 20:04:12 -0500

    feat: added theme to navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,18 +1,35 @@
 <script>
-  export let segment: string | undefined;
+  import { page } from '$app/stores';
+  import { theme } from '../store/store';
+
+  const changeMode = () => {
+    if ($theme === 'dark') {
+      theme.set('light') 
+    } else {
+      theme.set('dark') 
+    }
+  }
+  theme.subscribe((e) => {
+    console.log(e)
+  })
 </script>
 
-<style>
-</style>
 
-<nav class="flex justify-center h-25 bg-dark-800">
-  <ul class="flex items-center justify-between w-10/12 py-3">
-    <a href="." class="h-25">
-      <img class="h-full" src="wicho.png" alt="wicho" srcset="wicho.png">
+<nav class="flex justify-center h-25 xs:w-full md:w-9/12 mx-auto">
+  <ul class="flex items-center justify-between w-full py-3">
+    <a href="/" class="h-25">
+      <!-- <img class="h-full" src="wicho.png" alt="wicho"> -->
     </a>
     <div class="flex">
-      <li class="px-6 py-1 {segment === undefined ? 'bg-primary-500 text-white rounded' : 'text-primary-50'}"><a href="." sapper:noscroll>Inicio</a></li>
-      <li class="px-6 py-1 {segment === 'blog' ? 'bg-primary-500 text-white rounded' : 'text-primary-50'}"><a rel=prefetch href="blog" sapper:noscroll>Blog</a></li>
+      <li class="px-6 py-1 {$page.path === '/' ? 'bg-primary-500 text-white rounded' : 'text-surface-200'}">
+        <a href="/">Inicio</a>
+      </li>
+      <li class="px-6 py-1 {$page.path.includes('blog') ? 'bg-primary-500 text-white rounded' : 'text-surface-200'}">
+        <a sveltekit:prefetch rel=prefetch href="/blog">Blog</a>
+      </li>
+      <li class="px-6 py-1">
+        <button on:click="{changeMode}">toogle theme</button>
+      </li>
     </div>
   </ul>
 </nav>
