4ad65998a8fa159e173053489fa36c47b3f6b3e2
wicho1001 <wichoguerrero747@gmail.com>
2021-08-17 15:24:20 -0500

    chore: removed subscription

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -9,22 +9,19 @@
       theme.set('dark') 
     }
   }
-  theme.subscribe((e) => {
-    console.log(e)
-  })
 </script>
 
 
-<nav class="flex justify-center h-25 xs:w-full md:w-9/12 mx-auto">
+<nav class="flex justify-center h-25 xs:w-full md:w-8/12 mx-auto">
   <ul class="flex items-center justify-between w-full py-3">
     <a href="/" class="h-25">
       <!-- <img class="h-full" src="wicho.png" alt="wicho"> -->
     </a>
     <div class="flex">
-      <li class="px-6 py-1 {$page.path === '/' ? 'bg-primary-500 text-white rounded' : 'text-surface-200'}">
+      <li class="px-6 py-1 text-surface-900 rounded transition duration-700 ease-in-out {$page.path === '/' ? 'rounded border-b-2 border-primary-500' : ''}">
         <a href="/">Inicio</a>
       </li>
-      <li class="px-6 py-1 {$page.path.includes('blog') ? 'bg-primary-500 text-white rounded' : 'text-surface-200'}">
+      <li class="px-6 py-1 text-surface-900 rounded transition duration-700 ease-in-out {$page.path.includes('blog') ? 'rounded border-b-2 border-primary-500' : ''}">
         <a sveltekit:prefetch rel=prefetch href="/blog">Blog</a>
       </li>
       <li class="px-6 py-1">
