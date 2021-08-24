569a84b9792c3df5cdd1f38db8bd3eef8ebaeaf2
wicho1001 <wichoguerrero747@gmail.com>
2021-08-24 10:45:49 -0500

    feat: added node for building process

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 const noExternal = process.env.NODE_ENV === 'production' ? ['marked', 'gray-matter'] : [];
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
+		adapter: node(),
 		vite: { ssr: { noExternal } }
 
 	}
