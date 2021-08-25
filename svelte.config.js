8fe4480453136a8c1bd7243230339da165116f56
wicho1001 <wichoguerrero747@gmail.com>
2021-08-25 15:09:36 -0500

    chore: minor fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify(),
+		adapter: vercel(),
 
 	}
 };
