0cd2bb2391521038d8ee09bc7dd2b7fa6ab4c280
wicho1001 <wichoguerrero747@gmail.com>
2021-09-14 13:40:46 -0500

    chore: removed vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 import node from '@sveltejs/adapter-static';
-
-import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
